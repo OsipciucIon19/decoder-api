9146bc3f84eaee03b1198853d4e9d5d8080e1436
OsipciucIon19 <ion-osipciuc@mail.ru>
2023-06-29 12:44:53 +0300

    fix dockerfile

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use('/api', route);
+app.use(route);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- src/middleware/validate-hex-middleware.ts
+++ src/middleware/validate-hex-middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 module.exports = function (req: Request, res: Response, next: NextFunction) {
   const { hexPayload } = req.body;
-  console.log(hexPayload)
+
   // Check if the payload is a valid hexadecimal string
   const hexRegex = /^[0-9A-Fa-f]+$/;
   if (!hexPayload || !hexRegex.test(hexPayload)) {
