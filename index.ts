3e546b1b1d7aff32f1c1c0afb4d3b610e2e9bf92
OsipciucIon19 <71135284+OsipciucIon19@users.noreply.github.com>
2023-08-23 15:04:10 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const route = require('./src/router');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
